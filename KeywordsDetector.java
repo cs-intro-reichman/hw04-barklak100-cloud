b8388c58694816a0777228874b3aaa12f7bb2793
barklak100-cloud <barklak100@gmail.com>
2025-11-25 09:32:25 +0200

    done

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,19 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+            for(String sentence : sentences) {
+            for(String keyword : keywords) {
+
+            String lowerCaseSentence = sentence.toLowerCase();
+            String lowerCaseKeyword = keyword.toLowerCase();
+
+            if (lowerCaseSentence.contains(lowerCaseKeyword)) {
+
+                System.out.println(sentence); 
+                break;
+            }
+        }
     }
+  }
+
 }
