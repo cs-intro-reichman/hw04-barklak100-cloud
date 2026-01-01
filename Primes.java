394147069b3ba482b584daa5baaa2a81a91f8119
barklak100-cloud <barklak100@gmail.com>
2026-01-01 11:59:44 +0200

    new

--- Primes.java
+++ Primes.java
@@ -1,14 +1,21 @@
 public class Primes {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number as an argument.");
+            return;
+        }
+
         final int n = Integer.parseInt(args[0]);
         boolean[] prime = new boolean[n + 1];
 
+        // אתחול המערך - נניח שכולם ראשוניים
         int i = 2;
         while (i <= n) {
             prime[i] = true;
             i++;
         }
 
+        // אלגוריתם הנפה
         int p = 2;
         while (p * p <= n) {
             if (prime[p]) {
@@ -17,13 +24,11 @@ public class Primes {
                     prime[multiple] = false;
                     multiple = multiple + p;
                 }
-            
+            } 
             p++; 
-        }
-
+        } 
 
         System.out.println("Prime numbers up to " + n + ":");
-
         int primeCount = 0;
         int k = 2;
         while (k <= n) {
@@ -34,9 +39,8 @@ public class Primes {
             k++;
         }
         
-        int percentage = (int) (((double) primeCount / n) * 100);
+        double percentage = ((double) primeCount / n) * 100;
         System.out.println("There are " + primeCount + " primes between 2 and " + n +
-                " (" + percentage + "% are primes)");
-    } 
+                " (" + (int)percentage + "% are primes)");
     }
 }
